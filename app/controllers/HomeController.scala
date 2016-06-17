968bf897a496c1be481dc156f65fccf798fa14fd
Ruud <ruud.prein@gmail.com>
2016-06-17 10:49:03 +0200

    added api 2 api call to textrazor

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,34 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
+import play.api.libs.json.Json
+import play.api.libs.ws.WSClient
 import play.api.mvc._
 
+import scala.concurrent.ExecutionContext
+
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject()(ws: WSClient)(implicit exec: ExecutionContext) extends Controller {
 
   /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    * Create an Action to render an HTML page with a welcome message.
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
+  def index = Action.async {
+    ws.url("http://api.textrazor.com")
+      .withHeaders("x-textrazor-key" -> "62f5a302677fb1a5cef6b0834c45a16ae20a3be22683ada5f9cb5313")
+      .post(Map("text" -> Seq("Wortels zijn lekker. Maar komkommer is nog lekkerder dan de druiven."),
+        "extractors" -> Seq("entities")))
+      .map { response =>
+        Ok(Json.parse(response.body))
+      }
   }
 
 }
