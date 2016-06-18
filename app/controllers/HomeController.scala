dc984fc2397ad44cc661e42e5ffa591eb4cde43a
Ruud <ruud.prein@gmail.com>
2016-06-18 15:09:03 +0200

    extra actions

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,6 +2,8 @@ package controllers
 
 import javax.inject._
 
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.libs.json.Json
 import play.api.libs.ws.WSClient
 import play.api.mvc._
@@ -25,6 +27,39 @@ class HomeController @Inject()(ws: WSClient)(implicit exec: ExecutionContext) ex
     ws.url("http://api.textrazor.com")
       .withHeaders("x-textrazor-key" -> "62f5a302677fb1a5cef6b0834c45a16ae20a3be22683ada5f9cb5313")
       .post(Map("text" -> Seq("Wortels zijn lekker. Maar komkommer is nog lekkerder dan de druiven."),
+        "extractors" -> Seq("entities")))
+      .map { response =>
+        Ok(Json.parse(response.body))
+      }
+  }
+
+  /**
+    * Text to be analyzed by TextRazor
+    */
+  case class AnalyzeText(text: String)
+
+  val analyzeTextForm = Form(
+    mapping(
+      "text" -> nonEmptyText
+    )(AnalyzeText.apply)(AnalyzeText.unapply)
+  )
+
+  val extract = Action(parse.form(analyzeTextForm)) { implicit request =>
+    val analyzeTextData = request.body
+    val text = analyzeTextData.text
+    //Redirect(routes.HomeController.analyze(text))
+    Ok(text)
+  }
+
+  /**
+    * Create an Action to analyze data via TextRazor web service. Returns the result without modification.
+    *
+    * @return
+    */
+  def analyze(text: String) = Action.async {
+    ws.url("http://api.textrazor.com")
+      .withHeaders("x-textrazor-key" -> "62f5a302677fb1a5cef6b0834c45a16ae20a3be22683ada5f9cb5313")
+      .post(Map("text" -> Seq(text),
         "extractors" -> Seq("entities")))
       .map { response =>
         Ok(Json.parse(response.body))
